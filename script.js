551acfb8fc2d93b75dcedf4f259aaefa17935911
umutesililiane <umutesililiane4@gmail.com>
2023-08-08 04:45:05 +0530

    add delete notes functionality

--- script.js
+++ script.js
@@ -7,8 +7,13 @@ createBtn.addEventListener("click", ()=>{
     let inputBox = document.createElement("p");
     let img = document.createElement("img");
     inputBox.className = "input-box";
-    inputBox.setAttribute("contentediitable","true");
+    inputBox.setAttribute("contenteditable","true");
     img.src = "images/delete.png";
     notesContainer.appendChild (inputBox).appendChild(img);
 
+})
+notesContainer.addEventListener("click",function(e){
+    if(e.target.tagName  == "IMG") {
+        e.target.parentElement.remove();
+    }
 })
